7ab1e824b2eb785557001338647257a440d4d07f
Michail Vanis <hello@mikevanis.com>
2012-12-12 14:14:53 +0000

    added isInQuadrant and get avg direction

--- xcode/Quadrant.cpp
+++ xcode/Quadrant.cpp
@@ -18,9 +18,28 @@ Quadrant::Quadrant(VectorPoint *v1, VectorPoint *v2, VectorPoint *v3, VectorPoin
 }
 
 Vec2f Quadrant::getAverageDirection(Vec2f location) {
+    Vec2f dir1 = location - vector1->loc;
+    Vec2f dir2 = location - vector2->loc;
+    Vec2f dir3 = location - vector3->loc;
+    Vec2f dir4 = location - vector4->loc;
     
+    float dist1 = dir1.lengthSquared();
+    float dist2 = dir2.lengthSquared();
+    float dist3 = dir3.lengthSquared();
+    float dist4 = dir4.lengthSquared();
+    
+    float totalDist = dist1 + dist2 + dist3 + dist4;
+    
+    Vec2f averageDirection = dir1*(1-dist1/totalDist) + dir2*(1-dist2/totalDist) + dir3*(1-dist3/totalDist) + dir4*(1-dist4/totalDist);
+    
+    return averageDirection;
 }
 
 bool Quadrant::isInQuadrant(Vec2f location) {
-    
+    if(location.x >= vector1->loc.x && location.x < vector4->loc.x && location.y >= vector1->loc.y && location.y < vector4->loc.y) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
